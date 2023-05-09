bfd3733f5a1d442760bd41d5bc300317afb52b92
marianaozorio <mariana.athayde@gmail.com>
2023-05-09 12:51:42 -0300

    create router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
-import Header from './Components/Header/Header';
-import Logo from './Components/Header/Logo.png';
-import { HiShoppingCart } from "react-icons/hi";
-import './Pages/Sobre/Sobre';
-import Sobre from './Pages/Sobre/Sobre';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Homepage from './Pages/Homepage/Homepage';
 import MeuCarrinho from './Pages/MeuCarrinho/MeuCarrinho';
+import MinhaConta from './Pages/MinhaConta/MinhaConta';
+import Resumo from './Pages/Resumo/Resumo';
+import Sobre from './Pages/Sobre/Sobre';
 
 function App() {
   return (
     <div className="App">
-      <Header
-      logo={Logo}
-      name= "Nome do Site"
-      conta= "Minha Conta"
-      icone1= {<HiShoppingCart/>}
-      carrinho= "Meu Carrinho"
-      logout= "logout"
-
-      />
-      <Sobre/>
-      <MeuCarrinho/>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/meucarrinho' element={<MeuCarrinho />} />
+          <Route path='/minhaconta' element={<MinhaConta />} />
+          <Route path='/resumo' element={<Resumo />} />
+          <Route path='/sobre' element={<Sobre />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
