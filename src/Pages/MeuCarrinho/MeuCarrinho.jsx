419749365d73ed091ab98b2fe9ad5e7005ece54f
Iarima95 <ia.nery.viegas@gmail.com>
2023-05-09 12:37:06 -0300

    Page MeuCarrinho

--- src/Pages/MeuCarrinho/MeuCarrinho.jsx
+++ src/Pages/MeuCarrinho/MeuCarrinho.jsx
@@ -15,9 +15,48 @@ function MeuCarrinho() {
                 Veja aqui o seu histórico de pedidos
             </p>
             <br/>
+            <table className='tableCarrinho'>
+                <tr>
+                    <td>Data do Pedido</td>
+                    <td>Produtos</td>
+                    <td> Custo</td>
+                    <td>Statos do Pedido</td>
+                </tr>
+                <tr>
+                    <td>20/02/2023</td>
+                    <td>Caneca Bees</td>
+                    <td>70 Pólens</td>
+                    <td>Entregue</td>
+                </tr>
+                <tr>
+                    <td>20/02/2023</td>
+                    <td>Caneca Bees</td>
+                    <td>70 Pólens</td>
+                    <td>Entregue</td>
+                </tr>
+                <tr>
+                    <td>20/02/2023</td>
+                    <td>Caneca Bees</td>
+                    <td>70 Pólens</td>
+                    <td>Entregue</td>
+                </tr>
+            </table>
             
+            <p className='tituloTransfira'>
+                Não vai usar seus pólens? Transfira para outra pessoa! </p>
+            <br/>
+            <div className='presente'>
+            <label for="emailPresente">Digite o e-mail da pessoa que vai receber os pólens:</label>
+            <input type="e-mail" name="" id="emailPresente" class='input' />
 
+            <label for="quantidadePólens">Quantos pólens deseja enviar?</label>
+            <input type="number" name="" id="quantidadePólens" class='input' />
+            
+            
+            </div>
         </div>
+        
+
         
     )
 };
--- src/Pages/Sobre/Sobre.jsx
+++ src/Pages/Sobre/Sobre.jsx
@@ -14,7 +14,7 @@ function Sobre() {
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in ante nec odio rutrum cursus. Fusce pharetra a massa sit amet tempor. Donec quis placerat dui, et venenatis libero. Etiam semper felis eget tincidunt suscipit. Integer efficitur quam a nisi mattis vulputate. Nullam et ultrices ex.  </p>
             <br/>
             <h2 
-            className='tituloSobre'> Você Pode Ainda Espelhar Seus Pólens Por Aí! </h2>
+            className='tituloSobre'> Você pode ainda espelhar seus pólens por aí! </h2>
             <br/>
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in ante nec odio rutrum cursus. Fusce pharetra a massa sit amet tempor. Donec quis placerat dui, et venenatis libero. Etiam semper felis eget tincidunt suscipit. Integer efficitur quam a nisi mattis vulputate. Nullam et ultrices ex.  </p>
 
