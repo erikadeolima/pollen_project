006361aae668f68ccc16e544d1ace19578f57882
erikadeolima <erikadeo.lima@hotmail.com>
2023-05-09 16:53:04 -0300

    feat: creating context general functions

--- src/Context/Provider.js
+++ src/Context/Provider.js
@@ -1,10 +1,49 @@
 import React, { useState } from 'react';
 import storage from './Context';
+import userInfo from '../untils/userInfo';
 
 function Provider({ children }) {
 
-  const context = {
+  const [userName, setUserName] = useState("");
+  const [pollenBalance, setPollenBalance] = useState(0);
+  const [orderHistory, setOrderHistory] = useState([]);
 
+  const getUserName = () => {
+    const user = userInfo.userName;
+    setUserName(user);
+  };
+
+  const getPollenBalance = () => {
+    const pollen = userInfo.pollenBalance;
+    setPollenBalance(pollen);
+  };
+
+  const getOrderHistory = () => {
+    const orderHistory = userInfo.orderHistory;
+    setOrderHistory(orderHistory);
+  };
+
+  const updatePollenBalance = (pollens) => {
+    setPollenBalance(pollens);
+  };
+
+  const updateOrderHistory = (order) => {
+    setOrderHistory(order);
+    /* setOrderHistory((prevState) => {
+      return {
+        ...prevState,
+        order
+      };
+    }); */
+  };
+
+
+  const context = {
+    userName,
+    pollenBalance,
+    orderHistory,
+    updateOrderHistory,
+    updatePollenBalance
   };
 
   return (
