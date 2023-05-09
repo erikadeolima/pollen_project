f253b702120f7f95d9f4bed8d91605f26698b75a
erikadeolima <erikadeo.lima@hotmail.com>
2023-05-09 14:48:17 -0300

    feat: crating context and provider router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Provider from './Context/Provider';
 
 import Header from './Components/Header/Header';
 
@@ -11,18 +12,20 @@ import Sobre from './Pages/Sobre/Sobre';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/MeuCarrinho' element={<MeuCarrinho />} />
-          <Route path='/MinhaConta' element={<MinhaConta />} />
-          <Route path='/Resumo' element={<Resumo />} />
-          <Route path='/Sobre' element={<Sobre />} />
-        </Routes>
-      </Router>
-    </div>
+    <Provider>
+      <div className="App">
+        <Router>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/MeuCarrinho' element={<MeuCarrinho />} />
+            <Route path='/MinhaConta' element={<MinhaConta />} />
+            <Route path='/Resumo' element={<Resumo />} />
+            <Route path='/Sobre' element={<Sobre />} />
+          </Routes>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
