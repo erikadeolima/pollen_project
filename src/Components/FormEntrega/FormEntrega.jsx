01791e2c67aba5c47511ffccc061c65c447b0616
rayane-barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2023-05-10 05:13:52 +0000

    push: add

--- src/Components/FormEntrega/FormEntrega.jsx
+++ src/Components/FormEntrega/FormEntrega.jsx
@@ -4,24 +4,61 @@ import { useForm } from 'react-hook-form';
 
 function FormEntrega() {
 
-    const { register, handleSubmit, setValue, setFocus } = useForm();
-    const onSubmit = data => console.log(data);
-    console.log(errors);
+    const { register, handleSubmit, setValue } = useForm();
+
+    const onSubmit = (e) => {
+        console.log(e);
+    }
+
+    const checkCEP = (e) => {
+        const cep = e.target.value.replace(/\D/g, '');
+        console.log(cep);
+
+        fetch('https://viacep.com.br/ws/${cep}/json/)').then(res => res.json()).then(data => {
+            console.log(data);
+            setValue('endereco', data.logradouro);
+            setValue('bairro', data.bairro)
+            setValue('cidade', data.localidade);
+            setValue('estado', data.uf);
+        });
+    }
 
     return (
-        <form className='' onSubmit={handleSubmit(onSubmit)}>
-            <input type="number" placeholder='CEP'
-                {...register("CEP", { required: true, maxLength: 9 })} onBlur={checkCEP} />
-            <input type="text" placeholder='Endereco'
-                {...register("Endereço", { required: true, maxLength: 100 })} />
-            <input type="number" placeholder='Numero'
-                {...register("Número", {})} />
-            <input type="text" placeholder='Complemento'
-                {...register("Cidade", {})} />
-            <input type="text" placeholder='Estado'
-                {...register("Estado", {})} />
+        <form className='formulario-entrega' onSubmit={handleSubmit(onSubmit)}>
+            <label>
+                <input type="number" placeholder='CEP'
+                    {...register("cep")} onBlur={checkCEP} />
+            </label>
 
-            <input type="submit" />
+            <label>
+                <input type="text" placeholder='Endereco'
+                    {...register("endereco")} />
+            </label>
+            <label>
+                <input type="text" placeholder='Bairro'
+                    {...register("bairro")} />
+            </label>
+
+            <label>
+                <input type="number" placeholder='Numero'
+                    {...register("numero")} />
+            </label>
+            
+            <label>
+                <input type="text" placeholder='Complemento'
+                    {...register("complemento")} />
+            </label>
+
+            <label>
+                <input type="text" placeholder='Cidade'
+                    {...register("cidade")} />
+            </label>
+            
+            <label>
+                <input type="text" placeholder='Estado'
+                    {...register("estado")} />
+            </label>
+            <button type="submit">Enviar</button>
         </form>
     );
 
--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Homepage.css';
 import Banner from '../../Components/Banner/Banner';
+import FormEntrega from '../../Components/FormEntrega/FormEntrega';
 
 function Homepage() {
     return(
         <div>
             <Banner
             />
+
+            <FormEntrega />
         </div>
     )
 };
