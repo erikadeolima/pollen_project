249c0d614c212735f927361d5619665881effa66
Iarima95 <ia.nery.viegas@gmail.com>
2023-05-12 20:11:47 -0300

    update home alert

--- src/Components/CardDescriptionProduct/CardDescriptionProduct.jsx
+++ src/Components/CardDescriptionProduct/CardDescriptionProduct.jsx
@@ -71,7 +71,8 @@ function CardPhotoProduct({ id, name, src, pollens }) {
                 <div className='productShop'>
                     <HiCurrencyDollar className='cardIcon' />
                     <p>{pollens} pollens</p>
-                    < BsFillCartPlusFill className='cardIcon addProduct' onClick={addInCart} />
+                    < BsFillCartPlusFill className='cardIcon addProduct' onClick= {() => alert("Produto enviado para o carrinho!") } />
+                    
                 </div>
             </div>
         </div>
