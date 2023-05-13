ee91318618d023cb591e75caf6e26acbdc1629b0
erikadeolima <erikadeo.lima@hotmail.com>
2023-05-12 22:40:59 -0300

    feat: fixing bug

--- src/Components/CardDescriptionProduct/CardDescriptionProduct.jsx
+++ src/Components/CardDescriptionProduct/CardDescriptionProduct.jsx
@@ -44,6 +44,7 @@ function CardPhotoProduct({ id, name, src, pollens }) {
             quantity: unity,
             subTotal: pollens * unity
         });
+        // alert("Produto enviado para o carrinho!")
     };
 
     return (
@@ -71,8 +72,8 @@ function CardPhotoProduct({ id, name, src, pollens }) {
                 <div className='productShop'>
                     <HiCurrencyDollar className='cardIcon' />
                     <p>{pollens} pollens</p>
-                    < BsFillCartPlusFill className='cardIcon addProduct' onClick= {() => alert("Produto enviado para o carrinho!") } />
-                    
+                    < BsFillCartPlusFill className='cardIcon addProduct' onClick={() => addInCart()} />
+
                 </div>
             </div>
         </div>
